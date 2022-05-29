967d6cb36c9e7a5bde1c9a55ebdb7d39499a1890
marte31 <105121045+marte31@users.noreply.github.com>
2022-05-29 17:26:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var name = 0; name < names.length; name++) {
+
+    var firstLetter = names[name].charAt(0).toLowerCase();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[name]);
+    } else {
+      helloSpeaker.speak(names[name]);
+    }
+  }
+
+})();
